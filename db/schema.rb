df72b0aae79e6995c72890f45db6b253528d2708
Kieran Hayes <kieran.hayes@dkd.de>
2009-11-13 16:36:31 +0100

    Added tagging migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091112170806) do
+ActiveRecord::Schema.define(:version => 20091113153607) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -72,6 +72,23 @@ ActiveRecord::Schema.define(:version => 20091112170806) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "taggable_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "user_groups", :force => true do |t|
     t.string   "name"
