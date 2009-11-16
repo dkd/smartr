65f7735718e6dff71e1fb116ab0ed34c0ff6ce30
Kieran Hayes <kieran.hayes@dkd.de>
2009-11-16 18:16:55 +0100

    Added Paperclip Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  
+  #Plugins
   acts_as_authentic
+  has_attached_file :avatar, :styles => { :medium => "100x100#", :thumb => "48x48#", :tiny => "16x16#" }
   
   #Associations
   has_many :questions
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091116153208) do
+ActiveRecord::Schema.define(:version => 20091116163403) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -109,8 +109,8 @@ ActiveRecord::Schema.define(:version => 20091116153208) do
     t.string   "login"
     t.string   "crypted_password"
     t.string   "password_salt"
-    t.string   "persistence_token",                :null => false
-    t.integer  "login_count",       :default => 0, :null => false
+    t.string   "persistence_token",                  :null => false
+    t.integer  "login_count",         :default => 0, :null => false
     t.datetime "last_request_at"
     t.datetime "last_login_at"
     t.datetime "current_login_at"
@@ -120,6 +120,10 @@ ActiveRecord::Schema.define(:version => 20091116153208) do
     t.integer  "views"
     t.string   "email"
     t.integer  "reputation"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["last_request_at"], :name => "index_users_on_last_request_at"
