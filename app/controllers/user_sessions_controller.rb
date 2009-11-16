8da8f09fd43b5fdb408c032b1b6cda5780d85614
Kieran Hayes <kieran.hayes@dkd.de>
2009-11-16 21:32:17 +0100

    Fixed UserController

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,7 +11,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default account_url
+      redirect_back_or_default questions_path
     else
       render :action => :new
     end
