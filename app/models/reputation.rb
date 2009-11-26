e68cd5c2cbe365124ef54e9e22b9c091a9b82476
Kieran Hayes <kieran.hayes@dkd.de>
2009-11-26 15:38:42 +0100

    removed growl

--- app/models/reputation.rb
+++ app/models/reputation.rb
@@ -1,11 +1,11 @@
 require "rubygems"
-require 'ruby-growl' if RAILS_ENV == "development"
+#require 'ruby-growl' if RAILS_ENV == "development"
 
 class Tell
   attr_accessor :g
   def initialize(message)
-    g = Growl.new("localhost", "ruby-growl", ["ruby-growl Notification"], ["ruby-growl Notification"], nil) if RAILS_ENV == "development"
-    g.notify("ruby-growl Notification", "Reputation", message, 1, true) if RAILS_ENV == "development"
+  #  g = Growl.new("localhost", "ruby-growl", ["ruby-growl Notification"], ["ruby-growl Notification"], nil) if RAILS_ENV == "development"
+  #  g.notify("ruby-growl Notification", "Reputation", message, 1, true) if RAILS_ENV == "development"
   end
 end 
 
