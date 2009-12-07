7ce530837a799e964b10220d55967b981fdf6593
Kieran Hayes <kieran.hayes@dkd.de>
2009-12-07 15:33:15 +0100

    remove non matching tags from result

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   def index
     if params[:tag].present?
-      @tags = Question.tagged_with("#{params[:tag]}%").tag_counts
+      @tags = Question.tagged_with("#{params[:tag]}%").tag_counts.collect{ |t| t if t.name.match /#{params[:tag]}/ }.delete_if{ |t| t==nil }
     else
       @tags = Question.tag_counts
     end
