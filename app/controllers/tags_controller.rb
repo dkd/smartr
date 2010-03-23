b9da7a1be6de19e611e527fb8bc640ea64258ff1
Kieran Hayes <kieran.hayes@dkd.de>
2010-03-23 10:01:52 +0100

    moved question menu helper into show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,17 +2,26 @@ class TagsController < ApplicationController
   before_filter :require_user
   
   def index
-    if params[:tag].present?
-      @tags = Question.tagged_with("#{params[:tag]}%").tag_counts.collect{ |t| t if t.name.match /#{params[:tag]}/ }.delete_if{ |t| t==nil }
+    
+    if params[:q].present?
+      @tags = Question.tagged_with("#{params[:q]}%").tag_counts.collect{ |t| t if t.name.match /#{params[:q]}/ }.delete_if{ |t| t==nil }
     else
       @tags = Question.tag_counts
     end
     respond_to do |format|
+      
       format.html{}
+      
       format.js{
         render :partial => "list"
       }
+      format.json{
+        puts "Hallo"
+        render :json, @tags.to_json
+      }
     end
   end
-
+  
+  
+  
 end
