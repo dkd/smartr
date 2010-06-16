be8ed85b0215459dcede45dbc203ffb3739353b7
Kieran Hayes <kieran.hayes@dkd.de>
2010-06-16 17:48:52 +0200

    Added admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.namespace :admin do |admin|
+  map.namespace :admin do |admin|    
     admin.resources :comments
     admin.resources :answers
     admin.resources :questions
