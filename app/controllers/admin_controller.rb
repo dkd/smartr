bd2918e0c3017b202f5a08ce089bd245140474ff
Kieran Hayes <kieran.hayes@dkd.de>
2010-07-07 15:23:41 +0200

    Styled admin votes pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   before_filter :require_admin
   def index
-    @votes = Vote.find(:all,  :limit => 10, :order => 'updated_at desc')
+    @votes = Vote.find(:all, :order => 'updated_at desc').paginate :page => params[:page], :per_page => 15
   end
 end
