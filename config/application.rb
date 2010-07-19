6ffb929200427b261e55f1c4182a92f6079dcb48
Kieran Hayes <kieran1977@gmail.com>
2010-07-19 23:27:16 +0200

    Almost there, have to move searchable out of all model

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,11 @@ module Smartr
     config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
+     config.generators do |g|
+       g.orm             :active_record
+       g.template_engine :erb
     #   g.test_framework  :test_unit, :fixture => true
-    # end
+     end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
