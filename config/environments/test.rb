c90f84d3d06affb542590b97bd3768fb2429b766
Kieran Hayes <kieran.hayes@dkd.de>
2010-07-23 18:29:25 +0200

    Cucumber starts again, use rake cucumber RAILS_ENV=test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,5 +25,13 @@ Smartr::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.secret_token ='94b9c594695e69bdef6b1d4be037af5853be976b39a52a02f260fca0d0a36a8f913572bfdb631f55971a3b10b8dd9a875f9776ca61371741544e6ccc064dd41e'
+  Sunspot.config.solr.url = 'http://localhost:8981/solr'
+  
+  # Start Solr
+  # sunspot-solr start -p 8981 -d solr/data/test -s solr --pid-dir=tmp/pids -l FINE --log-file=log/sunspot-solr-test.log
+  # Stop Solr
+  # sunspot-solr stop -p 8981 -d solr/data/test -s solr --pid-dir=tmp/pids -l FINE --log-file=log/sunspot-solr-test.log
 end
