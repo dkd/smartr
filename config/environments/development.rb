5937dcef2e604aeb5b152bc72facb892d4c9d859
Kieran Hayes <kieran1977@gmail.com>
2010-07-27 23:31:11 +0200

    first glimpse of a the new look

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Smartr::Application.configure do
   config.action_mailer.perform_deliveries = true
   #config.gem "rails-footnotes"
   
+  config.active_support.deprecation = :log
+  
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.secret_token ='94b9c594695e69bdef6b1d4be037af5853be976b39a52a02f260fca0d0a36a8f913572bfdb631f55971a3b10b8dd9a875f9776ca61371741544e6ccc064dd41e'
