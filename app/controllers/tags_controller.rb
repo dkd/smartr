6f24d13254fa76daaef6aef099485c63b8425e68
Kieran Hayes <kieran.hayes@dkd.de>
2010-07-28 18:13:33 +0200

    minor tagging fixes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,12 @@
 class TagsController < ApplicationController
   
   def index
+    # Refactor
+    # select count(*), T.name from tags as T, taggings as G 
+    # WHERE g.tag_id = T.id
+    # GROUP by T.id
+    # ORDER BY
+    # count(*) DESC
     
     if params[:q].present?
       @tags = Question.tagged_with("#{params[:q]}%").tag_counts.collect{ |t| t if t.name.match /#{params[:q]}/ }.delete_if{ |t| t==nil }
