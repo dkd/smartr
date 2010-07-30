51a81a3ef92698f638b2d7361f6caeb362973a13
Kieran Hayes <kieran.hayes@dkd.de>
2010-07-30 18:38:09 +0200

    Quick fix for tag ajax search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,12 +8,9 @@ class TagsController < ApplicationController
       @tags = ActsAsTaggableOn::Tag.group('tags.id').joins(:taggings).select("count(*) as count,tags.id,tags.name").order('count(*) desc').paginate :page => params[:page], :per_page => 60
     end
     
-    respond_to do |format|
-      
-      format.html{}
-      format.js{ 
-        puts "yeah"
-        render "list"}
+    respond_to do |format|      
+      format.html{ puts "yeah html"}
+      format.js{ render :partial => "list"}
       format.json{ render :json => @tags.map { |tag| tag.name}.to_json }
     end
   end
