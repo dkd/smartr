5f9ece6299f8a04bf19da9e5ca6f3e3c49ec9dad
Kieran Hayes <kieran.hayes@dkd.de>
2011-01-17 18:07:59 +0100

    More tests for QuestionsController

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -69,9 +69,18 @@ describe QuestionsController do
    describe "POST question with correct parameters" do
       it "should redirect created question" do
         post :create, :question => {:name => "How many PHP Frameworks try to emulate Ruby on Rails?", :body => Faker::Lorem.sentences(10).to_s, :tag_list => "testing, rails, rspec"}
-        response.should be_redirect
+        response.should redirect_to(:controller => "questions", :action => "show", :id => Question.last.id, :friendly_id => Question.last.friendly_id)
+        assigns(:question).should eq(Question.last)
       end
     end
+    
+    describe "PUT question with wrong question owner" do
+       it "should redirect to question" do
+         put :update, :id => question.id
+         response.should redirect_to(:controller => "questions", :action => "show", :id => question.id, :friendly_id => question.friendly_id)
+         assigns(:question).should eq(question)
+       end
+     end
     
   end
 
@@ -79,7 +88,14 @@ describe QuestionsController do
     before do
       sign_in question.user
     end
-
+    
+   describe "PUT question with with incorrect parameters" do
+       it "should redirect to question" do
+         put :update, :id => question.id, :question => {:name => ""}
+         response.should render_template(:edit)
+         assigns(:question).should eq(question)
+       end
+     end
   end
   
 end 
