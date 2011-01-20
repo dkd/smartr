deb2d1f3540aa9d35947883b201bbb87928ed7f2
Kieran Hayes <kieran.hayes@dkd.de>
2011-01-20 18:24:59 +0100

    votesController spec (empty)

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,8 +3,25 @@ require "spec_helper"
 describe VotesController do
   include Devise::TestHelpers
   
-  describe "GET :who_is_online" do
-    
-  end
+  context "As a user who is" do
 
+    context "not logged in" do
+      describe "he votes on a question" do
+        it "and fails" do
+        end 
+      end
+    end
+    
+    context "logged in" do
+
+      describe "and votes up a question" do
+        describe "he already voted up" do
+          it "and fails" do
+          end
+        end
+      end
+
+    end
+
+  end
 end
