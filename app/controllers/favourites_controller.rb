bf2a8bf8d13b39ec2955acfa764be217b98e42a0
Kieran Hayes <kieran.hayes@dkd.de>
2011-01-26 16:26:14 +0100

    Controller specs for favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,6 +1,7 @@
 class FavouritesController < ApplicationController
-
+  respond_to :js, :html
   helper UsersHelper
+  before_filter :authenticate_user!, :only => [:toggle]
   
   def index
     @user = User.find params[:user_id]
@@ -9,37 +10,22 @@ class FavouritesController < ApplicationController
   end
   
   def toggle
-    @favourite = Favourite.find_by_user_id_and_question_id(@current_user.id, params[:id])
-    
+    @favourite = Favourite.find_by_user_id_and_question_id(current_user.id, params[:id])
+    @question_id = params[:id]
     respond_to do |format|
-      format.js { 
-        render :update do |page|
-          
-          if @favourite.blank?
-            page << "$('#favourite-question-#{params[:id]}').attr('class', 'favourite-saved')"
-            page << "$.gritter.add({
-                        	title: 'Favourite saved',              	
-                        	text: 'You successfully saved this question!',
-                        	time: 5000,
-                        	class_name: 'gritter-info'
-                        });"
-            @favourite = Favourite.new({:question_id => params[:id]})
-            @favourite.user = @current_user
-            @favourite.save
-          else
-            page << "$('#favourite-question-#{params[:id]}').attr('class', 'favourite')"
-            page << "$.gritter.add({
-                        	title: 'Favourite deleted',              	
-                        	text: 'You successfully removed this question from your favourtites!',
-                        	time: 5000,
-                        	class_name: 'gritter-info'
-                        });
-                    $('#user-favourite-question-#{params[:id]}').hide();"
-            @favourite.destroy
-            end
-          page
+      format.js {
+        if @favourite.blank?
+          @message = "You successfully saved this question!"
+          @class = "favourite-saved"
+          @favourite = Favourite.new({:question_id => @question_id})
+          @favourite.user = current_user
+          @favourite.save
+        else
+          @message = "You successfully deleted this question!"
+          @class = "favourite"
+          @favourite.destroy
         end
-        }
+      }
     end    
   end
   
