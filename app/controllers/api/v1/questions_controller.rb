5bfe4d10fc7fa05b86c843d01082d5b94e0edb9d
Kieran Hayes <kieran.hayes@dkd.de>
2011-01-26 17:17:39 +0100

    specs on api for questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,14 +1,7 @@
 class Api::V1::QuestionsController < ApplicationController
-  
+  respond_to :json
   def index
     @questions = Question.latest.paginate :page => params[:page], :per_page => 30
-    
-    respond_to do |format|
-      format.json{
-        render :json => @questions.to_json(:include => :user)
-      }
-    end
-    
+    render :json => @questions.to_json(:include => :user)
   end
-    
 end
---
+++ spec/requests/api/v1/questions_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Questions" do
+  describe "GET /api/v1/questions" do
+    it "works!" do
+      get api_v1_questions_path
+      
+    end
+  end
+end
