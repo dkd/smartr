e520fb975f9e3da0ff87384d9d4e098e89350351
Kieran Hayes <kieran.hayes@dkd.de>
2011-02-01 17:21:05 +0100

    another reputation test

--- spec/models/reputation_spec.rb
+++ spec/models/reputation_spec.rb
@@ -12,12 +12,42 @@ describe Reputation do
         end
         it "so the question has an accepted answer, but the owner gets no extra points" do
           Reputation.toggle_acceptance(question, question.answers.first)
+          question.reload
           question.accepted_answer.should == question.answers.first
+          question.user.reputation.should == 0
         end
       end       
  
-      describe "revokes acceptance" do
-
+      describe "revokes acceptance of an answer" do
+        before do
+          question.answers << Factory.build(:answer, :user => Factory.create(:user2))
+          question.save
+          Reputation.toggle_acceptance(question, question.answers.last)
+        end
+        it "so the question has no accepted answer anymore and the answer owner loses reputation" do
+          question.accepted_answer.user.reputation.should == Smartr::Settings[:reputation][:answer][:accept]
+          answer_user = question.accepted_answer.user
+          question.reload
+          Reputation.toggle_acceptance(question, question.answers.last)
+          question.reload
+          answer_user.reload
+          question.accepted_answer.should be_nil
+          answer_user.reputation.should == 0
+        end
+      end
+ 
+      describe "revokes acceptance of his own answer" do
+        before do
+          question.answers << Factory.build(:answer, :user => question.user)
+          question.save
+          Reputation.toggle_acceptance(question, question.answers.last)
+        end
+        it "so the question has no accepted answer anymore" do 
+          Reputation.toggle_acceptance(question, question.answers.last)
+          question.reload
+          question.accepted_answer.should be_nil
+          question.user.reputation.should == 0
+        end
       end
 
       describe "accepts an answer" do
@@ -27,6 +57,7 @@ describe Reputation do
         end
         it "so the question has an accepted answer and the answer owner gains some reputation" do
           Reputation.toggle_acceptance(question, question.answers.first)
+          question.reload
           question.accepted_answer.should == question.answers.first
           question.user.should_not == question.accepted_answer.user
           question.accepted_answer.user.reputation.should == Smartr::Settings[:reputation][:answer][:accept]
