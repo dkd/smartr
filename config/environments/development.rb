efca11ab0f338a369da1cee1334d2e26488cc675
Kieran Hayes <kieran.hayes@dkd.de>
2011-02-04 17:51:53 +0100

    using inject to count votes on question

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ Smartr::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
-  #config.gem "rails-footnotes"
   
   config.active_support.deprecation = :log
   
