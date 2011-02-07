f4168edf6671af405df9c3f04304b3ce027fbd3b
Kieran Hayes <kieran.hayes@dkd.de>
2011-02-07 18:23:12 +0100

    some request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe "Users" do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "works!" do
       get users_path
+      response.should be_successful
     end
   end
 end
