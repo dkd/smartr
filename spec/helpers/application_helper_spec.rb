8ed9516833734500c8762d97cc207dd6140334d8
Kieran Hayes <kieran1977@gmail.com>
2011-02-09 19:00:28 +0100

    admin/users controller specs

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+  
+  describe "direction" do
+    it "returns up" do
+      helper.direction(1).should == "up"
+    end
+    it "returns down" do
+      helper.direction("sdsad").should == "down"
+    end
+  end
+end  
