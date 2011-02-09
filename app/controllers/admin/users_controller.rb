6a5775318a24e279ce968b1da5f070e589856b27
Kieran Hayes <kieran1977@gmail.com>
2011-02-09 20:40:14 +0100

    merging tests

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,34 +1,19 @@
 class Admin::UsersController < ApplicationController
+  before_filter :require_admin
+  
   # GET /admin_users
-
   def index
     @users = User.all
-    respond_to do |format|
-      format.html # index.html.erb
-
-    end
   end
 
   # GET /admin_users/1
-
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-
-    end
   end
 
   # GET /admin_users/new
-
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-
-    end
   end
 
   # GET /admin_users/1/edit
@@ -37,47 +22,32 @@ class Admin::UsersController < ApplicationController
   end
 
   # POST /admin_users
-  # POST /admin_users.xml
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
       if @user.save
         flash[:notice] = 'Admin::User was successfully created.'
-        format.html { redirect_to(@user) }
-
+          redirect_to(admin_user_url(@user))
       else
-        format.html { render :action => "new" }
-
+        render :action => "new"
       end
-    end
   end
 
   # PUT /admin_users/1
-
   def update
     @user = User.find(params[:id])
-
-    respond_to do |format|
       if @user.update_attributes(params[:user])
         flash[:notice] = 'Admin::User was successfully updated.'
-        format.html { redirect_to(@user) }
-
+        redirect_to(admin_user_url(@user))
       else
-        format.html { render :action => "edit" }
-
+        render :action => "edit"
       end
-    end
   end
 
   # DELETE /admin_users/1
-
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(admin_users_url) }
-    end
+    redirect_to(admin_users_url)
   end
+
 end
