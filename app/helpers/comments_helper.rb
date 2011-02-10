cc431f1e51ab5561f86e1948039d4e7a05aa87d9
Kieran Hayes <kieran1977@gmail.com>
2011-02-11 00:59:34 +0100

    better comments_helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,13 @@
 module CommentsHelper
-  def comments_order_menu(active)
+  def comments_order_menu(active, anchor)
+      @question ||= @parent
       menu = []
-      menu << {:name => I18n.t(:"comments.order.latest"), :id => "latest", :link => "#{current_url}?comments_order=latest" }
-      menu << {:name => I18n.t(:"comments.order.reputation"), :id => "reputation", :link => "#{current_url}?comments_order=reputation" }
+      menu << { :name => I18n.t(:"comments.order.latest"), 
+                :id => "latest", 
+                :link => question_url(@question, :id => @question.id, :comments_order => "latest", :anchor => anchor) }
+      menu << { :name => I18n.t(:"comments.order.reputation"), 
+                :id => "reputation",
+                :link => question_url(@question, :id => @question.id, :comments_order => "reputation", :anchor => anchor)  }
       build_menu(menu, active)
   end
 end
