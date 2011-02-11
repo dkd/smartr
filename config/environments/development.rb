e49376a619111e83809cf005c5edabe5142292e4
Kieran Hayes <kieran.hayes@dkd.de>
2011-02-11 10:37:55 +0100

    doh!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ Smartr::Application.configure do
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
-
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   
