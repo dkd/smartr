da43d2e898be55bf92f15ade14a8bcfd75c8cc1b
Kieran Hayes <kieran1977@gmail.com>
2011-02-13 23:24:43 +0100

    some work on voting

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,9 +3,14 @@ module CommentsHelper
       @question ||= @parent
       menu = []
       menu << { :id => "latest", 
-                :link => link_to (I18n.t(:"comments.order.latest"), comments_path(:model => commentable.class.name, :id => commentable.id, :comments_order => "latest"), :remote => true) }
+                :link => link_to(I18n.t(:"comments.order.latest"), 
+                comments_path(:model => commentable.class.name, 
+                              :id => commentable.id, 
+                              :comments_order => "latest"), 
+                              :remote => true, 
+                              :class => ("active" unless (session[:comments_order] != active))) }
       menu << { :id => "reputation", 
-                :link => link_to (I18n.t(:"comments.order.reputation"), comments_path(:model => commentable.class.name, :id => commentable.id, :comments_order => "reputation"), :remote => true) }
+                :link => link_to(I18n.t(:"comments.order.reputation"), comments_path(:model => commentable.class.name, :id => commentable.id, :comments_order => "reputation"), :remote => true, :class => "state") }
      build_ajax_menu(menu, active)
   end
 end
