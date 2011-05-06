b29b8652d22ee944c9aab5e04a6f06fd463b9067
Kieran Hayes <kieran.hayes@dkd.de>
2011-05-06 17:46:59 +0200

    Now at 79% coverage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:edit, :update]
-  before_filter :is_admin?, :only => [:admin]
 
   def index
     @users = User.reputation.paginate :page => params[:page], :per_page => 10
@@ -9,16 +8,19 @@ class UsersController < ApplicationController
 
   def search
     if params[:q].present?
-      @users = User.find(:all, :conditions => ["login like ?","%#{params[:q]}%"], :order => "reputation desc").paginate :page => params[:page], :per_page => 10
+      @users = User.search(params[:q])
     else
-      @users = User.find(:all, :order => "reputation desc").paginate :page => params[:page], :per_page => 10
+      @users = User.paginate :page => params[:page], :per_page => 10
+    end
+    respond_to do |format|
+      format.js {}
+      format.html { render :index }
     end
   end
 
   def who_is_online
     @users = User.online
   end
-
 
   def reputation
     @user = User.find(params[:id])
