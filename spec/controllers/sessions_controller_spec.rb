cfde9712e4d7913a25dd0fef554fd83473def7a0
Kieran Hayes <kieran1977@gmail.com>
2011-05-26 14:46:31 +0200

    added SessionsController test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe SessionsController do
   include Devise::TestHelpers
   render_views
+  let (:user) { Factory(:endless_user)}
   before do
     request.env["devise.mapping"] = Devise.mappings[:user]
   end
@@ -10,20 +11,38 @@ describe SessionsController do
     describe "GET new" do
       it "shows the login page" do
         get :new
-          response.should be_success
+        response.should be_success
+      end
+
+    end
+    context "invalid params" do
+      describe "POST create" do
+        it "should show the login form" do
+          post :create
+          response.should render_template("users/sessions/new")
+          flash[:alert].should eq(I18n.t("devise.failure.invalid"))
+        end
+      end 
+    end
+    context "valid params" do
+      describe "POST create" do
+        it "should redirect to the homepage" do
+          post :create, :user => { :login => user.login, :password => "leanderTaler3000" }
+          response.should redirect_to root_url
+        end
       end
     end
   end
+
   context "authorized" do
-    let (:user) { Factory(:endless_user)}
+    before do
+      sign_in user
+    end
     describe "GET new" do
-      before do
-        sign_in user
-      end
       it "shows the login page" do
         get :new
-          response.should redirect_to root_url
-          flash[:alert].should eq(I18n.t("devise.failure.already_authenticated"))
+        response.should redirect_to root_url
+        flash[:alert].should eq(I18n.t("devise.failure.already_authenticated"))
       end
     end
   end
